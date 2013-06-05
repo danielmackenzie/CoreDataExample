484932b5da00f4b42a34619e053dbeb8b3e9f161
Daniel MacKenzie <danielmackenzie@Daniel-MacKenzies-MacBook-Pro.local>
2013-06-04 21:11:23 -0400

    Initial Commit

---
+++ CoreDataExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CoreDataExample
+//
+//  Created by Daniel MacKenzie on 2013-06-04.
+//  Copyright (c) 2013 Daniel MacKenzie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
